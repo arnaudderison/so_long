e58342afca7c3527dc1fcb6a8baf20020e60e0e3
arnaudderison <arnaud.derison@hotmail.com>
2024-07-24 19:03:55 +0200

    is valid file

---
+++ src/utils/maps/is_valid_map.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_valid_map.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aderison <aderison@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 15:01:20 by aderison          #+#    #+#             */
+/*   Updated: 2024/07/24 16:00:55 by aderison         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Un player
+Une Sortie
+Au moins un collectible
+*/
+#include "so_long.h"
+
+static void	nb_items(char *line, int *items)
+{
+	int	i;
+
+	i = -1;
+	while (++i < ft_strlen(line))
+	{
+		if (line[i] == 'C')
+			items[0] += 1;
+		if (line[i] == 'P')
+			items[1] += 1;
+		if (line[i] == 'E')
+			items[2] += 1;
+	}
+}
+
+int	is_valid_map(t_list **plan)
+{
+	t_list	*map;
+	int		items[3];
+
+	if (!plan || !*plan)
+		return (false);
+	map = *plan;
+	items[0] = 0;
+	items[1] = 0;
+	items[2] = 0;
+	while (map)
+	{
+		if (((char *)map->content)[0] != '1'
+			|| ((char *)map->content)[ft_strlen(map->content) - 2] != '1')
+			return (0);
+		nb_items(map->content, items);
+		map = map->next;
+	}
+	return (items[0] > 0 && items[1] == 1 && items[2] == 1);
+}
