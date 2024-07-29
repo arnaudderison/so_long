78c7f06b3a3846e6c9987ce5b7c125a5c54b8762
arnaudderison <arnaud.derison@hotmail.com>
2024-07-29 20:43:11 +0200

    great fct

--- src/maps/astar.c
+++ src/maps/astar.c
@@ -6,7 +6,7 @@
 /*   By: aderison <aderison@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 05:35:43 by aderison          #+#    #+#             */
-/*   Updated: 2024/07/25 05:47:04 by aderison         ###   ########.fr       */
+/*   Updated: 2024/07/26 03:32:16 by aderison         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,19 @@ static void	init_node_details(t_astar *astar, t_window *win)
 			astar->node_details[i][j].parent.col = -1;
 		}
 	}
+}
+
+void	add_to_open_list(t_astar *astar, t_point point, t_node node)
+{
+	astar->open_list[astar->open_size] = point;
+	astar->node_details[point.row][point.col] = node;
+	astar->open_size++;
+}
+
+void	update_node(t_node *node, float f, float g, float h, t_point parent)
+{
+	node->f = f;
+	node->g = g;
+	node->h = h;
+	node->parent = parent;
 }
